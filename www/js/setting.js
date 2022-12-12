10cffdedeea0561453340dcf77a622bcbf15731a
karena-12 <20203018-fukuiyuta@hcs.ac.jp>
2022-12-12 12:15:56 +0900

    設定

--- www/js/setting.js
+++ www/js/setting.js
@@ -1,2 +1,11 @@
 // This is a JavaScript file
 
+// 設定画面のログアウト押下
+function logout(){
+    var result = window.confirm("ログアウトしますか？");
+
+    if(result){
+        alert('ログアウトします');
+        location.href = 'login.html';
+    }
+}

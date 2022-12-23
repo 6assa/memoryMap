b22e37701fe6478cfa8f583fdfae4e64f8fbdd44
github-pv <20203021-asakishinya@hcs.ac.jp>
2022-12-23 12:22:06 +0900

    年明け前最後

--- www/js/main.js
+++ www/js/main.js
@@ -6,8 +6,26 @@ var ncmb = new NCMB(appKey, clientKey);
 // NCMB.Objectのサブクラスを生成
 var post = ncmb.DataStore("post");
 
-// クラスの新しいインスタンスを生成
-var post = new post();
+// // クラスの新しいインスタンスを生成
+// var post = new post();
+
+post.fetchAll()
+    .then(function(results) {
+        console.log("取得成功:" + JSON.stringify(results));
+        var tempArray = [];
+        var arr = [];
+        $.each(results, function(cnt, value_data){
+            // 一次元配列に格納
+            tempArray = ['img/home.svg', value_data.userName, value_data.    postedMessage, value_data.postedDate];
+
+            // 二次元配列に格納
+            arr.push(tempArray);
+        });
+        console.log(arr);
+    })
+    .catch(function(error) {
+        console.log("取得失敗:" + JSON.stringify(error))
+    });
 
 
 // フォローの投稿を配列に定義
@@ -35,7 +53,7 @@ var openArray = [
 
 // フォローの投稿を表示
 function followPost() {
-    $.each(followArray, function(cnt, value_data){
+    $.each(arr, function(cnt, value_data){
         var content = document.getElementById('follow-content');
         var add_code = '<div class="board-item"><div class="icon-img"><img class="board-icon" src="' + value_data[0] + '" width="50px" height="50px" ></div><div class="board-text"><p id="text"><span>' + value_data[1] + '</span><br><span>' + value_data[2] + '</span></p><div class="post-img"><img src="img/share.png"><img src="img/good.png"></div></div><div class="post-time"><p class="time">' + value_data[3] + '</p></div></div>'
         content.insertAdjacentHTML('beforeend', add_code);

65240cdc584d99c2caa7d6dcd8fd2de5718ced03
Yuasa41 <20202041-yuasaatsutaka@hcs.ac.jp>
2023-01-17 14:46:37 +0900

    画像プレビュ

--- www/js/upload.js
+++ www/js/upload.js
@@ -3,67 +3,100 @@ var clientKey = "05557971c5c7770f388a7c460cdaa0362d55ab58b08ac0e27ee8abcc86c22aa
 
 var ncmb = new NCMB(appKey, clientKey);
 
+$('#addBtn').click(function () {
+    $(this).find('.img-input').click();
+});
+
 function onUploadBtn() {
     var text = document.getElementById("area");
     var photo = document.getElementById("preview");
 }
 
 function count_up(obj) {
-            var element = document.getElementById('inputlength');
-            var area = document.getElementById('area');
-            element.innerHTML = obj.value.length + "/300";
+    var element = document.getElementById('inputlength');
+    var area = document.getElementById('area');
+    element.innerHTML = obj.value.length + "/300";
 
-            if (obj.value.length > 300) {
-                element.style.color = 'red';
-            } else {
-                element.style.color = 'grey';
-            }
-        }
+    if (obj.value.length > 300) {
+        element.style.color = 'red';
+    } else {
+        element.style.color = 'grey';
+    }
+}
 
-        function disp() {
+function disp() {
 
-            // 「OK」時の処理開始 ＋ 確認ダイアログの表示
-            if (window.confirm('投稿しますか？')) {
+    // 「OK」時の処理開始 ＋ 確認ダイアログの表示
+    if (window.confirm('投稿しますか？')) {
 
-                location.href = "main.html"; // example_confirm.html へジャンプ
+        location.href = "main.html"; // example_confirm.html へジャンプ
 
-            }
-            // 「OK」時の処理終了
+    }
+    // 「OK」時の処理終了
 
-            // 「キャンセル」時の処理開始
-            else {
+    // 「キャンセル」時の処理開始
+    else {
 
-                window.alert('キャンセルされました'); // 警告ダイアログを表示
+        window.alert('キャンセルされました'); // 警告ダイアログを表示
 
-            }
-            // 「キャンセル」時の処理終了
+    }
+    // 「キャンセル」時の処理終了
 
-        }
+}
 
 
-        var file;
-        var url;
+var files = new Array();
 
-        function loadImage(obj) {
+function imgPreView(event, num) {
+    var file = event.target.files[0];
+    var reader = new FileReader();
 
-            for (i = 0; i < obj.files.length; i++) {
-                // 画像ファイルを取得・プレビュー表示
-                file = obj.files[i];
-                var fileReader = new FileReader();
-                fileReader.onload = (function(e) {
-                    document.getElementById('preview').innerHTML += "<img src=" + e.target.result + ">";
-                });
-                fileReader.readAsDataURL(file);
-            }
-            // スクロール初期位置指定
-            document.getElementById('preview').scrollLeft = 0;
-        };
+    var preview = document.getElementById("preview" + num);
+    var previewImg = document.getElementById("previewImage" + num);
 
-        /**
-         * ⇩写真挿入ボタン写真削除ボタン⇩
-         * 写真が入っているときはimg/photoDelete.svg
-         * 写真が入っていないときはimg/photoUpload.svg
-         * ⇩写真を挿入したいエリア⇩
-         * upload.htmlのclass="photo"
-         * 投稿ボタン現状何も動作しないです
-         */
+
+
+    if (previewImg != null) {
+        preview.removeChild(previewImg);
+    }
+
+    reader.onload = function (event) {
+        var img = document.createElement("img");
+        img.setAttribute("src", reader.result);
+        img.setAttribute("id", "previewImage" + num);
+        img.setAttribute("class", "previewImage");
+        preview.appendChild(img);
+        
+    };
+
+    document.getElementById("btn" + num).innerHTML = '<img class="addBtn" id="remBtn"' + num + ' src="img/photoDelete.svg" onclick="removePreview(' + num + ')">';
+
+
+
+
+    
+    
+
+
+    reader.readAsDataURL(file);
+    files.push(document.getElementById("previewImage" + num).getAttribute("src"));
+    console.log(files);
+}
+
+function removePreview(num) {
+    var preview = document.getElementById("preview" + num);
+    var previewImg = document.getElementById("previewImage" + num);
+    preview.removeChild(previewImg);
+    document.getElementById("btn" + num).innerHTML = '<label id="lebel' + num + '"><img class="addBtn" id="addBtn' + num + '" src="img/photoUpload.svg"><input id="input' + num + '" class="img-input" onchange="imgPreView(event,' + num + ')" type="file"></label>';
+    delete files[num - 1];
+    console.log(files);
+}
+
+/**
+ * ⇩写真挿入ボタン写真削除ボタン⇩
+ * 写真が入っているときはimg/photoDelete.svg
+ * 写真が入っていないときはimg/photoUpload.svg
+ * ⇩写真を挿入したいエリア⇩
+ * upload.htmlのclass="photo"
+ * 投稿ボタン現状何も動作しないです
+ */

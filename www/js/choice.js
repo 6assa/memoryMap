c4e5fc2a879be18f2800ca471b799b0ed0665595
Yuasa41 <20202041-yuasaatsutaka@hcs.ac.jp>
2023-01-18 14:13:52 +0900

    選択画面で選んだボタンに枠線つけたよ

--- www/js/choice.js
+++ www/js/choice.js
@@ -4,7 +4,10 @@ let select
 
 $('.select-btn').click(function (e) {
     // クリック要素内のテキストを取得
+    $('.select-btn').css('border','none');
     select = $(this).find('#select-text').text();
+    $(this).css('border','5px solid rgba(77, 166, 255,0.5)');
+    $(this).css('border-radius','12px');
     $('#inroom-btn').prop('disabled',false);
     // 入室ボタン押下イベント
     $('#inroom-btn').click(function () {

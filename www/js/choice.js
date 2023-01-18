7f82395f7de6f7c494a8de228bd8d8f6a5eae2e0
Yuasa41 <20202041-yuasaatsutaka@hcs.ac.jp>
2023-01-18 13:38:47 +0900

    選択画面の機能できた

--- www/js/choice.js
+++ www/js/choice.js
@@ -5,6 +5,7 @@ let select
 $('.select-btn').click(function (e) {
     // クリック要素内のテキストを取得
     select = $(this).find('#select-text').text();
+    $('#inroom-btn').prop('disabled',false);
     // 入室ボタン押下イベント
     $('#inroom-btn').click(function () {
         // フェードアウト後に遷移

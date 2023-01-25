c65f0d97b3d3c642fe4bd3f7359091ca5ded1d18
6assa <20202032-ikenotsubasa@hcs.ac.jp>
2023-01-25 09:23:23 +0900

    いいね

--- www/js/main.js
+++ www/js/main.js
@@ -39,7 +39,7 @@ $(window).on('load', async function () {
             $.each(result, function (cnt, value_data) {
                 var object = result[cnt];
                 var content = document.getElementById('follow-content');
-                var add_code = '<div class="board-item"><div class="icon-img"><img class="board-icon" id="image' + count +'" src="img/good.png" width="50px" height="50px" /></div><div class="board-text"><p id="text"><span>' + object.displayName + '</span><br><span>' + object.postedMessage + '</span></p><div class="post-img"><img src="img/share.png"><img src="img/good.png"></div></div><div class="post-time"><p class="time">' + object.postedDate + '</p></div></div>'
+                var add_code = '<div class="board-item"><div class="icon-img"><img class="board-icon" id="image' + count +'" src="img/good.png" width="50px" height="50px" /></div><div class="board-text"><p id="text"><span>' + object.displayName + '</span><br><span>' + object.postedMessage + '</span></p><div class="reaction"><div class="post-img"><img src="img/share.png"></div><input type="checkbox" class="like" id="like"><label for="like"><svg id="svg" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24"><path d="M12 21.35l-1.45-1.32C5.4 15.36 2 12.28 2 8.5 2 5.42 4.42 3 7.5 3c1.74 0 3.41.81 4.5 2.09C13.09 3.81 14.76 3 16.5 3 19.58 3 22 5.42 22 8.5c0 3.78-3.4 6.86-8.55 11.54L12 21.35z"/></svg></label></div></div><div class="post-time"><p class="time">' + object.postedDate + '</p></div></div>'
                 content.insertAdjacentHTML('beforeend', add_code);
                 image[cnt] = "userIcon.svg";
                 console.log(cnt);
@@ -62,6 +62,10 @@ $(window).on('load', async function () {
         });
         setImg(count);
 });
+
+$(document).on('change','.like',function(){
+    $(this).find('#svg').toggleClass('like');
+})
 
 // タブ押下による投稿の表示・非表示
 function tabs() {

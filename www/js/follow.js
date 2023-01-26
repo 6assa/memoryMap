f58138876b717d52bc1170e2f189210c7aa08b08
Yuasa41 <20202041-yuasaatsutaka@hcs.ac.jp>
2023-01-26 14:47:50 +0900

    フォローモーダル作成

---
+++ www/js/follow.js
@@ -0,0 +1,13 @@
+// This is a JavaScript file
+$(
+    $('#open-content .board-item .icon-img').click(function(){
+        $('#follow-modal').fadeIn('fast');
+    })
+
+    
+)
+$(
+    $('#close').click(function () {
+        $('#follow-modal').fadeOut('fast');
+    })
+)

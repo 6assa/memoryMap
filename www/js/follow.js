f07be4d43d32f0a821bec3e38793bfbd9988c4ae
Yuasa41 <20202041-yuasaatsutaka@hcs.ac.jp>
2023-02-04 15:46:18 +0900

    フォローモーダルレイアウトだけ完成

--- www/js/follow.js
+++ www/js/follow.js
@@ -15,47 +15,6 @@ var follow = ncmb.DataStore("follow");
 // ユーザ情報テーブル
 var user = ncmb.DataStore("users");
 
-$(window).on('load',function(){
-    $('#follow-modal').find('.follow-btn').hide();
-    // 投稿したユーザのメールアドレスを取得
-    var select_user=$(this).find('#userId').val();
-    console.log($(this).find('#userId').val());
-    user.equalTo('mailAddress', select_user).fetch().then(result => {
-
-        $('#display-name').text(result['displayName']);
-        $('#user-name').text(result['mailAddress']);
-        $('#myself').text(result['selfComment']);
-        icon_url = result['iconUrl'];
-
-        // プロフィールアイコン画像の適用
-        document.getElementById("icon-img").src = "https://mbaas.api.nifcloud.com/2013-09-01/applications/dzkz4P3WqMDSGgc3/publicFiles/" + icon_url;
-    });
-    
-
-
-    if (select_user != loginUserName) {
-        // フォローしているかを結果の件数で判定
-        follow.equalTo('followingUserName', loginUserName).equalTo('userName', select_user).count().fetchAll().then(results => {
-            console.log(results);
-            console.log(results.count);
-            if (results.count === 0) {
-                // まだフォローしていない時
-                $('#follow-modal').find('.follow').show();
-                
-            } else if (results.count > 0) {
-                // フォロー中の時
-                $('#follow-modal').find('.follow-out').show();
-            }
-
-        }).catch(function (err) {
-            console.log(err);
-        });
-
-        $('#follow-modal').show();
-    }else{
-        console.log("同じユーザ");
-    }
-})
 
 
 
